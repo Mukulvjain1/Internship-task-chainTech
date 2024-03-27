762cf7f15437d360d9a15661a3b9fbe330276d9f
mukulvjain1 <mukulvjain1@gmail.com>
2024-03-27 16:40:39 +0530

    second commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const sideBar = document.querySelector(".sidebar");
+const menu = document.querySelector("#menu");
+
+function openSidebar() {
+  sideBar.style.display = "flex";
+}
+const closeSideBar = () => {
+  sideBar.style.display = "none";
+};
